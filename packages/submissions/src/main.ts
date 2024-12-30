a142b716956b5778f92667204a6a59587299dd24
Pedro Gomes <pcgbernardes@gmail.com>
2024-12-30 11:26:06 -0400

    feat(config):bootstrap app with configService

--- packages/submissions/src/main.ts
+++ packages/submissions/src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import { type Env } from './infra/env'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+  const configService = app.get<ConfigService<Env, true>>(ConfigService)
+  const port = configService.get('PORT', { infer: true })
+  await app.listen(port)
 }
-bootstrap();
+
+void bootstrap()
