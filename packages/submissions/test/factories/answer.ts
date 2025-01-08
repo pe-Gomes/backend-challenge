5040c13903cfc54a1b90d6275cffd7e18df2dabe
Pedro Gomes <pcgbernardes@gmail.com>
2025-01-07 21:01:09 -0400

    refactor(test): add prisma factory for answer & challenge

--- packages/submissions/test/factories/answer.ts
+++ packages/submissions/test/factories/answer.ts
@@ -1,6 +1,12 @@
 import { EntityID } from '@/core/value-objects/entity-id'
-import { Answer, type AnswerEntityProps } from '@/domain/classroom/entities/answer'
+import {
+  Answer,
+  type AnswerEntityProps,
+} from '@/domain/classroom/entities/answer'
+import { PrismaAnswerMapper } from '@/infra/db/prisma/mappers/prisma-answer-mapper'
+import { PrismaService } from '@/infra/db/prisma/prisma.service'
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function createAnswer(
   overrides: Partial<AnswerEntityProps> = {},
@@ -14,4 +20,19 @@ export function createAnswer(
     },
     id,
   )
+}
+
+@Injectable()
+export class AnswerFactory {
+  constructor(private db: PrismaService) {}
+
+  async makePrismaAnswer(data: Partial<AnswerEntityProps> = {}, id?: string) {
+    const answer = createAnswer(data, new EntityID(id))
+
+    await this.db.answer.create({
+      data: PrismaAnswerMapper.toPersistence(answer),
+    })
+
+    return answer
+  }
 }
--- packages/submissions/test/factories/challenge.ts
+++ packages/submissions/test/factories/challenge.ts
@@ -1,9 +1,12 @@
-import { type EntityID } from '@/core/value-objects/entity-id'
+import { EntityID } from '@/core/value-objects/entity-id'
 import {
   Challenge,
   type ChallengeEntityProps,
 } from '@/domain/classroom/entities/challenge'
+import { PrismaChallengeMapper } from '@/infra/db/prisma/mappers/prisma-challenge-mapper'
+import { PrismaService } from '@/infra/db/prisma/prisma.service'
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function createChallenge(
   overrides: Partial<ChallengeEntityProps> = {},
@@ -18,4 +21,22 @@ export function createChallenge(
     },
     id,
   )
+}
+
+@Injectable()
+export class ChallengeFactory {
+  constructor(private db: PrismaService) {}
+
+  async makePrismaChallenge(
+    data: Partial<ChallengeEntityProps> = {},
+    id?: string,
+  ) {
+    const challenge = createChallenge(data, new EntityID(id))
+
+    await this.db.challenge.create({
+      data: PrismaChallengeMapper.toPersistence(challenge),
+    })
+
+    return challenge
+  }
 }
