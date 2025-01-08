957787ab947720765573338189606b7b2f7b2081
Pedro Gomes <pcgbernardes@gmail.com>
2025-01-07 21:08:15 -0400

    feat(env):add kafka & database environment to schema

--- packages/submissions/src/infra/env/index.ts
+++ packages/submissions/src/infra/env/index.ts
@@ -5,6 +5,9 @@ export const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'test', 'production'])
     .default('development'),
+  DATABASE_URL: z.string(),
+  KAFKA_BROKERS: z.string().default('localhost:9092'),
+  KAKFA_CHALLENGE_CONSUMER: z.string().default('challenge-consumer'),
 })
 
 export type Env = z.infer<typeof envSchema>
